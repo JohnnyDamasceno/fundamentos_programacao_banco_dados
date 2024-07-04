18407e4be33adffa21ff6e25e735405c079b8c19
Johhny Dias <147009274+JohnnyDamasceno@users.noreply.github.com>
2024-07-03 21:39:16 -0300

    Add files via upload

---
+++ Condicional.py
@@ -0,0 +1,24 @@
+#print('Olá mundo!')
+
+#idade = 16 
+
+#if idade == 16:
+    #print('Menor de idade')
+
+
+#year = int(input('Informe a sua idade:  '))
+
+#if year <= 16:
+    #print('Você é menor de idade.')
+#else:
+    #print('Você é maior de idade.')
+
+
+fet = int(input('Informe a sua idade:'))
+
+if fet > 18:
+ print('Filme de terror')
+elif fet >=14:
+ print('Filme De Ação')
+else:
+ print('Filme Infatil')             
---
+++ Lacosderepeticao.py
@@ -0,0 +1,23 @@
+#X = 0
+#while X < 5:
+    #nome = input('Qual é o seu nome?')
+    #x=x+1
+
+
+
+#X = 0
+
+#while x < 5:
+    #nome = input ('qual é o seu nome?')
+    #x=x+1
+
+
+x = 0                                                                                               
+animais = []
+
+while x < 5:
+    nome = input ('Quais são os nomes dos animais?')
+    animais.append(nome)
+    x=x+1
+
+print(animais)
